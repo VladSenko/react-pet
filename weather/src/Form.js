9a1bec9f90efefa40c16da9aa6f16a70d554bccc
vlad-senko <vlad.senko@icloud.com>
2020-07-10 00:27:41 +0300

    Control input value by component state

--- weather/src/Form.js
+++ weather/src/Form.js
@@ -1,6 +1,15 @@
 import React from 'react';
 
 class Form extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            inputValue: '',
+        };
+    }
+
+    updateInputValue = (e) => this.setState({ inputValue: e.target.value });
+
     render() {
         return (
             <form className='city-form'>
@@ -8,7 +17,11 @@ class Form extends React.Component {
                     type='text'
                     placeholder='Search by city'
                     className='city-input'
+                    required
+                    value={this.state.inputValue}
+                    onChange={this.updateInputValue}
                 ></input>
+                <button className='search-button'>Search</button>
             </form>
         );
     }
